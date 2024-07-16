b3abc2eb67badddfbe3d857728ff695477733d45
vaibzInc <vaibhav@i.inc>
2024-07-16 19:36:33 +0530

    Set max_sidebar_width to 300

--- app/constant.ts
+++ app/constant.ts
@@ -53,7 +53,7 @@ export enum StoreKey {
 }
 
 export const DEFAULT_SIDEBAR_WIDTH = 300;
-export const MAX_SIDEBAR_WIDTH = 500;
+export const MAX_SIDEBAR_WIDTH = 300;
 export const MIN_SIDEBAR_WIDTH = 230;
 export const NARROW_SIDEBAR_WIDTH = 100;
 
